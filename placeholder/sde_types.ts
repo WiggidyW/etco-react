81c9c25d70fb0b4679681250368fc016f8d5a73f
WiggidyW <36451315+WiggidyW@users.noreply.github.com>
2023-10-11 20:36:36 -0500

    Delete placeholder directory

--- placeholder/sde_types.ts
+++
@@ -1,7 +0,0 @@
-import * as sdt from "./types";
-
-// pre-build placeholder
-export const SDE_TYPE_DATA: sdt.SDETypes = [];
-export const GROUP_NAMES: sdt.GroupNames = [];
-export const CATEGORY_NAMES: sdt.CategoryNames = [];
-export const MARKET_GROUP_NAMES: sdt.MarketGroupNames = [];
