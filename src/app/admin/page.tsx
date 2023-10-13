ed6ab75ab5b6a84c79c45f51578d6134b841374c
Wiggidy <rlr0017@auburn.edu>
2023-10-13 14:21:36 -0500

    add basic /admin page

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,10 @@
+import { serverCookiesGetCurrentCharacter } from "@/cookies/server";
+import { AdminMain } from "@/components/Main";
+import { ReactElement } from "react";
+
+const PATH = "/admin";
+
+export default function Page(): ReactElement {
+  const character = serverCookiesGetCurrentCharacter();
+  return <AdminMain path={PATH} character={character} />;
+}
