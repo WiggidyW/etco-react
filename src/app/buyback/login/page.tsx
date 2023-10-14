517336156fce519a32792aa1ea2bcbd23e7a40e9
Wiggidy <rlr0017@auburn.edu>
2023-10-13 19:23:42 -0500

    make PATH constant (as in other pages)

--- src/app/buyback/login/page.tsx
+++ src/app/buyback/login/page.tsx
@@ -1,6 +1,8 @@
 import LoginPage from "@/app/[...path]/page";
 import { ReactNode } from "react";
 
+const PATH_ARRAY = ["buyback", "login"];
+
 export default function Page(): ReactNode {
-  return <LoginPage params={{ path: ["buyback", "login"] }} />;
+  return <LoginPage params={{ path: PATH_ARRAY }} />;
 }
--- src/app/shop/login/page.tsx
+++ src/app/shop/login/page.tsx
@@ -1,6 +1,8 @@
-import LoginPage from "../../[...path]/page";
+import LoginPage from "@/app/[...path]/page";
 import { ReactNode } from "react";
 
+const PATH_ARRAY = ["shop", "login"];
+
 export default function Page(): ReactNode {
-  return <LoginPage params={{ path: ["shop", "login"] }} />;
+  return <LoginPage params={{ path: PATH_ARRAY }} />;
 }
