5fd02e2731a5836b160004d48289e7b1888d6c9f
Wiggidy <rlr0017@auburn.edu>
2023-10-14 17:01:03 -0500

    add retry attempts

--- genstaticdata_run.ts
+++ genstaticdata_run.ts
@@ -3,11 +3,21 @@ dotenv.config();
 
 import { genStaticData } from "./genstaticdata";
 
+const MAX_RETRY_ATTEMPTS: number = 3;
+const RETRY_SLEEP_MS: number = 5000;
+
 (async () => {
-  try {
-    await genStaticData("src/proto/staticdata");
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
+  for (let i = 0; i < MAX_RETRY_ATTEMPTS; i++) {
+    try {
+      await genStaticData("src/proto/staticdata");
+      console.log("generated static data");
+      process.exit(0);
+    } catch (e) {
+      console.error(e);
+    }
+    console.log(`retrying in ${RETRY_SLEEP_MS / 1000} seconds...`);
+    await new Promise((resolve) => setTimeout(resolve, RETRY_SLEEP_MS));
   }
+  console.error("failed to generate static data");
+  process.exit(1);
 })();
