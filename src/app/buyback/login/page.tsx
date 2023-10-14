eb7c578a434b5cf26d6c160ac99fc147e1b22023
Wiggidy <rlr0017@auburn.edu>
2023-10-13 19:20:16 -0500

    inherit default Login functionality

--- src/app/buyback/login/page.tsx
+++ src/app/buyback/login/page.tsx
@@ -1,11 +1,6 @@
-import { UserLogin } from "@/components/Login/Login";
-import { AuthMain } from "@/components/Main";
+import LoginPage from "../../[...path]/page";
 import { ReactNode } from "react";
 
 export default function Page(): ReactNode {
-  return (
-    <AuthMain>
-      <UserLogin path={["buyback", "login"]} />
-    </AuthMain>
-  );
+  return <LoginPage params={{ path: ["buyback", "login"] }} />;
 }
