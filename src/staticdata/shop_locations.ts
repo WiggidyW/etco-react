23696c06f3a63405710c835e0aa201bc1a3b84ce
WiggidyW <36451315+WiggidyW@users.noreply.github.com>
2023-10-14 22:18:27 -0500

    Delete src/staticdata/shop_locations.ts

--- src/staticdata/shop_locations.ts
+++
@@ -1,6 +0,0 @@
-import * as sdt from "./types";
-
-// pre-build placeholder
-export const SHOP_LOCATIONS: sdt.ShopLocations = {};
-export const SHOP_REGION_NAMES: sdt.RegionNames = {};
-export const SHOP_SYSTEM_NAMES: sdt.SystemNames = {};
