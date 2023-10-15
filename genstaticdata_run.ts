f82e22c85d10501bec19ce265af91f25fe7ad116
Wiggidy <rlr0017@auburn.edu>
2023-10-14 20:51:14 -0500

    fix invalid path

--- genstaticdata_run.ts
+++ genstaticdata_run.ts
@@ -9,7 +9,7 @@ const RETRY_SLEEP_MS: number = 5000;
 (async () => {
   for (let i = 0; i < MAX_RETRY_ATTEMPTS; i++) {
     try {
-      await genStaticData("src/proto/staticdata");
+      await genStaticData("src/staticdata");
       console.log("generated static data");
       process.exit(0);
     } catch (e) {
