cf5d4fe1990625d99fc6f0644eea7e51c3b9aa58
Wiggidy <rlr0017@auburn.edu>
2023-10-14 21:52:45 -0500

    add exports

--- src/staticdata/sde_types.js
+++ src/staticdata/sde_types.js
@@ -3,3 +3,5 @@ const SDE_TYPE_DATA = [];
 const GROUP_NAMES = [];
 const CATEGORY_NAMES = [];
 const MARKET_GROUP_NAMES = [];
+
+module.exports = { SDE_TYPE_DATA, GROUP_NAMES, CATEGORY_NAMES, MARKET_GROUP_NAMES };
--- src/staticdata/shop_locations.js
+++ src/staticdata/shop_locations.js
@@ -2,3 +2,5 @@
 const SHOP_LOCATIONS = {};
 const SHOP_REGION_NAMES = {};
 const SHOP_SYSTEM_NAMES = {};
+
+module.exports = { SHOP_LOCATIONS, SHOP_REGION_NAMES, SHOP_SYSTEM_NAMES };
