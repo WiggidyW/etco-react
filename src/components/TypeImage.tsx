05f57ea4e87f532c84b155d07eb17fc764b48027
Wiggidy <rlr0017@auburn.edu>
2023-10-15 20:16:30 -0500

    override negative type IDs

--- src/components/TypeImage.tsx
+++ src/components/TypeImage.tsx
@@ -9,16 +9,20 @@ export const TypeImage = ({
   typeId,
   width = 24,
 }: TypeImageProps): ReactElement => (
-  <img
-    className={classNames(
-      "type-image",
-      "object-cover",
-      "mr-1",
-      "h-full",
-      "aspect-[1/1]"
+  <>
+    {typeId >= 0 && (
+      <img
+        className={classNames(
+          "type-image",
+          "object-cover",
+          "mr-1",
+          "h-full",
+          "aspect-[1/1]"
+        )}
+        src={`https://images.evetech.net/types/${typeId}/icon?size=32`}
+        alt={typeId.toString()}
+        width={width}
+      />
     )}
-    src={`https://images.evetech.net/types/${typeId}/icon?size=32`}
-    alt={typeId.toString()}
-    width={width}
-  />
+  </>
 );
