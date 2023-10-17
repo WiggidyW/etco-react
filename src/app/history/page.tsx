3d89246cff2d0124379fccfebaf9460c32a5531d
Wiggidy <rlr0017@auburn.edu>
2023-10-17 18:21:22 -0500

    add history page (home -> history)

---
+++ src/app/history/page.tsx
@@ -0,0 +1,23 @@
+import { ErrorBoundaryRefresh } from "@/components/ErrorBoundary";
+import { Loading } from "@/components/Loading";
+import { LoggedInMain, Main } from "@/components/Main";
+import { UserQueueLoader } from "@/components/Queue/Loader";
+import { serverCookiesGetCurrentCharacter } from "@/cookies/server";
+import { ReactElement, Suspense } from "react";
+
+const PATH = "/history";
+
+export default function Page(): ReactElement {
+  const character = serverCookiesGetCurrentCharacter();
+  return (
+    <LoggedInMain path={PATH} character={character}>
+      {character && (
+        <Suspense fallback={<Loading scale="25%" />}>
+          <ErrorBoundaryRefresh>
+            <UserQueueLoader token={character.refreshToken} />
+          </ErrorBoundaryRefresh>
+        </Suspense>
+      )}
+    </LoggedInMain>
+  );
+}
