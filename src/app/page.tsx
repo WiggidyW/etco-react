a45ceda42086eb37c800c80b6d6e1b71481e0680
Wiggidy <rlr0017@auburn.edu>
2023-10-17 18:44:22 -0500

    make home different from history

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,10 @@
-import { ErrorBoundaryRefresh } from "@/components/ErrorBoundary";
-import { Loading } from "@/components/Loading";
 import { Main } from "@/components/Main";
-import { UserQueueLoader } from "@/components/Queue/Loader";
 import { serverCookiesGetCurrentCharacter } from "@/cookies/server";
-import { ReactElement, Suspense } from "react";
+import { ReactElement } from "react";
 
 const PATH = "/";
 
 export default function Page(): ReactElement {
   const character = serverCookiesGetCurrentCharacter();
-  return (
-    <Main path={PATH} character={character}>
-      {character && (
-        <Suspense fallback={<Loading scale="25%" />}>
-          <ErrorBoundaryRefresh>
-            <UserQueueLoader token={character.refreshToken} />
-          </ErrorBoundaryRefresh>
-        </Suspense>
-      )}
-    </Main>
-  );
+  return <Main path={PATH} character={character} />;
 }
