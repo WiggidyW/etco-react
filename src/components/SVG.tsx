ec63dbe0bc5e68a6ef133427500de58585a6eb07
Wiggidy <rlr0017@auburn.edu>
2023-10-23 15:16:05 -0500

    add search

--- src/components/SVG.tsx
+++ src/components/SVG.tsx
@@ -39,3 +39,22 @@ export const CopyClipboard = (props: SVGProps): ReactElement => (
     />
   </svg>
 );
+
+export const SearchIcon = (props: SVGProps): ReactElement => (
+  <svg {...props} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <circle
+      cx="10"
+      cy="10"
+      r="6"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M14.5 14.5L19 19"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
