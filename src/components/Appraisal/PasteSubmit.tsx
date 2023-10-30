589af0e2dc243f09cd00e4dda794eb63ddf2d271
Wiggidy <rlr0017@auburn.edu>
2023-10-30 18:46:01 -0500

    un-nest children component + use state prop

--- src/components/Appraisal/PasteSubmit.tsx
+++ src/components/Appraisal/PasteSubmit.tsx
@@ -7,7 +7,7 @@ import {
   TextArea,
 } from "../Input/Manipulator";
 import { experimental_useFormStatus as useFormStatus } from "react-dom";
-import { ReactElement, useState } from "react";
+import { ReactElement, useCallback, useState } from "react";
 import { Loading } from "../Loading";
 import classNames from "classnames";
 
@@ -15,106 +15,116 @@ export interface PasteSubmitProps {
   className?: string;
   action: (text: string, systemId: number) => Promise<void>;
   options: { label: string; value: string }[];
+  text: string | null;
+  setText: (text: string | null) => void;
+  system: SelectOption<string> | null;
+  setSystem: (system: SelectOption<string> | null) => void;
 }
 export const PasteSubmit = ({
   action,
   className,
-  options,
+  ...childrenProps
 }: PasteSubmitProps): ReactElement => {
-  //
-  const PasteSubmitFormChildren = (): ReactElement => {
-    const [system, setSystem] = useState<SelectOption<string> | null>(null);
-    const [text, setText] = useState<string | null>(null);
-    const { pending } = useFormStatus();
-
-    const reset = () => {
-      setSystem(null);
-      setText(null);
-    };
-
-    return (
-      <>
-        {pending && (
-          <div className={classNames("absolute", "inset-0", "z-30")}>
-            <Loading scale="75%" />
-          </div>
-        )}
-        <div className={classNames("flex-grow", "w-full")}>
-          <TextArea
-            value={text}
-            setValue={setText}
-            title="Paste Items"
-            name="text"
-            required
-            wFull
-            hFull
-            disabled={pending}
-          />
-        </div>
-        <div className={classNames("flex", "w-full")}>
-          <span className={classNames("flex-shrink")}>
-            <SelectInput
-              value={system}
-              setValue={setSystem}
-              title="System"
-              disabled={pending}
-              name="systemId"
-              options={options}
-              required
-              wFull
-            />
-          </span>
-          <span className={classNames("ml-auto")} />
-          <span
-            className={classNames(
-              "border",
-              "border-light-red-base",
-              "hover:border-light-red-active",
-              "self-end",
-              "ml-2"
-            )}
-          >
-            <Button
-              type="reset"
-              disabled={pending}
-              className={classNames("relative", "h-10", "p-0", "px-2")}
-              onClick={reset}
-            >
-              Reset
-            </Button>
-          </span>
-          <span
-            className={classNames(
-              "border",
-              "border-light-blue-base",
-              "hover:border-light-blue-active",
-              "self-end",
-              "ml-2"
-            )}
-          >
-            <Button
-              type="submit"
-              disabled={pending || !system || !text}
-              className={classNames("relative", "h-10", "p-0", "px-2")}
-            >
-              Submit
-            </Button>
-          </span>
-        </div>
-      </>
-    );
-  };
+  const formAction = useCallback(
+    (formData: FormData) => {
+      const systemId = Number(formData.get("systemId") as string);
+      const text = formData.get("text") as string;
+      return action(text, systemId);
+    },
+    [action]
+  );
 
   return (
-    <form
-      className={classNames("relative", className)}
-      action={(formData) => {
-        const systemId = Number(formData.get("systemId") as string);
-        const text = formData.get("text") as string;
-        return action(text, systemId);
-      }}
-    >
-      <PasteSubmitFormChildren />
+    <form className={classNames("relative", className)} action={formAction}>
+      <PasteSubmitFormChildren {...childrenProps} />
     </form>
+  );
+};
+
+type PasteSubmitChildrenProps = Omit<PasteSubmitProps, "action" | "className">;
+const PasteSubmitFormChildren = ({
+  options,
+  text,
+  setText,
+  system,
+  setSystem,
+}: PasteSubmitChildrenProps): ReactElement => {
+  const { pending } = useFormStatus();
+
+  const reset = useCallback(() => {
+    setSystem(null);
+    setText(null);
+  }, [setSystem, setText]);
+
+  return (
+    <>
+      {pending && (
+        <div className={classNames("absolute", "inset-0", "z-30")}>
+          <Loading scale="75%" />
+        </div>
+      )}
+      <div className={classNames("flex-grow", "w-full")}>
+        <TextArea
+          value={text}
+          setValue={setText}
+          title="Paste Items"
+          name="text"
+          required
+          wFull
+          hFull
+          disabled={pending}
+        />
+      </div>
+      <div className={classNames("flex", "w-full")}>
+        <span className={classNames("flex-shrink")}>
+          <SelectInput
+            value={system}
+            setValue={setSystem}
+            title="System"
+            disabled={pending}
+            name="systemId"
+            options={options}
+            required
+            wFull
+          />
+        </span>
+        <span className={classNames("ml-auto")} />
+        <span
+          className={classNames(
+            "border",
+            "border-light-red-base",
+            "hover:border-light-red-active",
+            "self-end",
+            "ml-2"
+          )}
+        >
+          <Button
+            type="reset"
+            disabled={pending}
+            className={classNames("relative", "h-10", "p-0", "px-2")}
+            onClick={reset}
+          >
+            Reset
+          </Button>
+        </span>
+        <span
+          className={classNames(
+            "border",
+            "border-light-blue-base",
+            "hover:border-light-blue-active",
+            "self-end",
+            "ml-2"
+          )}
+        >
+          <Button
+            type="submit"
+            disabled={pending || !system || !text}
+            className={classNames("relative", "h-10", "p-0", "px-2")}
+          >
+            Submit
+          </Button>
+        </span>
+      </div>
+    </>
   );
 };
