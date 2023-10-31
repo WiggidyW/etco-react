cf9fff860a93ecba4f25701abd07ea7aa7ca18ee
Wiggidy <rlr0017@auburn.edu>
2023-10-30 19:01:43 -0500

    simplify props

--- src/components/Appraisal/ShopAppraisalContainer.tsx
+++ src/components/Appraisal/ShopAppraisalContainer.tsx
@@ -1,28 +1,24 @@
-import { LocationSelect } from "./LocationSelect";
+import { LocationSelect, LocationSelectProps } from "./LocationSelect";
 import { AppraisalContainer } from "./Container";
 import { ReactElement } from "react";
 import classNames from "classnames";
 import { AppraisalContainerChildren } from "./ContainerChildren";
-import { ICharacter } from "@/browser/character";
 
-export interface ShopAppraisalContainerProps {
-  options: { label: string; value: string }[];
+export interface ShopAppraisalContainerProps
+  extends Omit<LocationSelectProps, "className"> {
   containerChildren?: AppraisalContainerChildren;
-  character: ICharacter | null | undefined;
 }
 export const ShopAppraisalContainer = ({
   containerChildren,
-  options,
-  character,
+  ...locationSelectProps
 }: ShopAppraisalContainerProps): ReactElement => {
   if (containerChildren === undefined) {
     return (
       <>
         <div className={classNames("h-[5%]")} />
         <LocationSelect
-          character={character}
           className={classNames("ml-auto", "mr-auto")}
-          options={options}
+          {...locationSelectProps}
         />
       </>
     );
@@ -30,9 +26,8 @@ export const ShopAppraisalContainer = ({
     return (
       <AppraisalContainer containerChildren={containerChildren}>
         <LocationSelect
-          character={character}
           className={classNames("w-96", "justify-self-start")}
-          options={options}
+          {...locationSelectProps}
         />
       </AppraisalContainer>
     );
